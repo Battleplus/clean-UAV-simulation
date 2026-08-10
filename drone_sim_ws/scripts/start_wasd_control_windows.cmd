f86ffb4c6647b01caa8a67d42a5832f12910cfd4
Battleplus <3559424769@qq.com>
2026-08-10 20:05:12 +0800

    fix: launch WASD bridge in focusable console

--- drone_sim_ws/scripts/start_wasd_control_windows.cmd
+++ drone_sim_ws/scripts/start_wasd_control_windows.cmd
@@ -1,4 +1,8 @@
 @echo off
+if /i not "%~1"=="--conhost" (
+  start "" "%SystemRoot%\System32\conhost.exe" "%ComSpec%" /d /c ""%~f0" --conhost"
+  exit /b
+)
 title my_drone WASD control
 echo Starting my_drone WASD controller in Ubuntu-24.04...
 python "%~dp0windows_wasd_bridge.py"
